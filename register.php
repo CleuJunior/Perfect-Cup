5c6d7107e3321aadfc8fbbb1025cfc42aa1e02e4
CleuJunior <cleu.junior@gmail.com>
2021-02-06 00:50:07 -0300

    Ajax e Validacoes

--- register.php
+++ register.php
@@ -18,8 +18,12 @@
     <link href="css/business-casual.css" rel="stylesheet">
 
     <!-- Fonts -->
-    <link href="http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800" rel="stylesheet" type="text/css">
-    <link href="http://fonts.googleapis.com/css?family=Josefin+Slab:100,300,400,600,700,100italic,300italic,400italic,600italic,700italic" rel="stylesheet" type="text/css">
+    <link
+        href="http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800"
+        rel="stylesheet" type="text/css">
+    <link
+        href="http://fonts.googleapis.com/css?family=Josefin+Slab:100,300,400,600,700,100italic,300italic,400italic,600italic,700italic"
+        rel="stylesheet" type="text/css">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -27,6 +31,79 @@
         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Script -->
+    <script type="text/javascript">
+    $(document).ready(function() {
+
+        $("#register").click(function() {
+
+            fname = $("#fname").val();
+            lname = $("#lname").val();
+            email = $("#email").val();
+            password = $("#password").val();
+
+            $.ajax({
+                type: "POST",
+                url: "adduser.php",
+                data: "fname=" + fname + "&lname=" + lname + "&email=" + email + "&password=" +
+                    password,
+                success: function(html) {
+                    if (html == 'true') {
+
+                        $("#add_err2").html('<div class="alert alert-success"> \
+                                                 <strong>Account</strong> processed. \ \
+                                                 </div>');
+
+                        window.location.href = "index.php";
+
+                    } else if (html == 'false') {
+                        $("#add_err2").html('<div class="alert alert-danger"> \
+                                                 <strong>Email Address</strong> already in system. \ \
+                                                 </div>');
+
+                    } else if (html == 'fname') {
+                        $("#add_err2").html('<div class="alert alert-danger"> \
+                                                 <strong>First Name</strong> is required. \ \
+                                                 </div>');
+
+                    } else if (html == 'lname') {
+                        $("#add_err2").html('<div class="alert alert-danger"> \
+                                                 <strong>Last Name</strong> is required. \ \
+                                                 </div>');
+
+                    } else if (html == 'eshort') {
+                        $("#add_err2").html('<div class="alert alert-danger"> \
+                                                 <strong>Email Address</strong> is required. \ \
+                                                 </div>');
+
+                    } else if (html == 'eformat') {
+                        $("#add_err2").html('<div class="alert alert-danger"> \
+                                                 <strong>Email Address</strong> format is not valid. \ \
+                                                 </div>');
+
+                    } else if (html == 'pshort') {
+                        $("#add_err2").html('<div class="alert alert-danger"> \
+                                                 <strong>Password</strong> must be at least 4 characters . \ \
+                                                 </div>');
+
+                    } else {
+                        $("#add_err2").html('<div class="alert alert-danger"> \
+                                                 <strong>Error</strong> processing request. Please try again. \ \
+                                                 </div>');
+                    }
+                },
+                beforeSend: function() {
+                    $("#add_err2").html("loading...");
+                }
+            });
+            return false;
+        });
+    });
+    </script>
 
 </head>
 
@@ -36,11 +113,9 @@
     <div class="address-bar">3481 Melrose Place | Beverly Hills, CA 90210 | 123.456.7890</div>
 
     <!-- Navigation -->
-   <?php require_once 'nav.php'; ?>
+    <?php require_once 'nav.php'; ?>
 
     <div class="container">
-
-       
         <div class="row">
             <div class="box">
                 <div class="col-lg-12">
@@ -48,28 +123,30 @@
                     <h2 class="intro-text text-center">Registration
                         <strong>form</strong>
                     </h2>
+                    <div id="add_err2"></div>
                     <hr>
                     <form role="form">
                         <div class="row">
                             <div class="form-group col-lg-4">
                                 <label>First Name</label>
-                                <input type="text" id="fname" name="fname" class="form-control" maxlength="25">
+                                <input type="text" id="fname" name="fname" maxlength="25" class="form-control">
                             </div>
                             <div class="form-group col-lg-4">
                                 <label>Last Name</label>
-                                <input type="text" id="lname" name="lname" class="form-control" maxlength="25">
+                                <input type="text" id="lname" name="lname" maxlength="25" class="form-control">
                             </div>
                             <div class="form-group col-lg-4">
                                 <label>Email Address</label>
-                                <input type="email" id="email" name="email" class="form-control" maxlength="25">
+                                <input type="email" id="email" name="email" maxlength="25" class="form-control">
                             </div>
                             <div class="clearfix"></div>
                             <div class="form-group col-lg-12">
                                 <label>Password</label>
-                                <input type="password" id="password" name="passwrod" class="form-control" maxlength="10">
+                                <input type="password" id="password" name="password" maxlength="10"
+                                    class="form-control">
                             </div>
                             <div class="form-group col-lg-12">
-                                <button type="submit" class="btn btn-default" id="register">Submit</button>
+                                <button type="submit" id="register" class="btn btn-default">Submit</button>
                             </div>
                         </div>
                     </form>
@@ -89,9 +166,6 @@
             </div>
         </div>
     </footer>
-
-    <!-- jQuery -->
-    <script src="js/jquery.js"></script>
 
     <!-- Bootstrap Core JavaScript -->
     <script src="js/bootstrap.min.js"></script>
