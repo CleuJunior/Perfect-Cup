b86465b4a5b3778738f5f9acaac97994ad4f8545
CleuJunior <cleu.junior@gmail.com>
2021-02-06 12:57:26 -0300

    Validacao prt 2

--- blog.php
+++ blog.php
@@ -1,3 +1,12 @@
+<?php session_start();
+
+    if(isset($_SESSION['login'])){
+        $fname = $_SESSION['fname'];
+        $lname = $_SESSION['lname'];
+    
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -47,6 +56,9 @@
         <div class="row">
             <div class="box">
                 <div class="col-lg-12">
+
+                    <h2 class="text-center">Welcome <?=$fname . " " . $lname;?> - <a href="logout.php">Logout</a></h2>
+
                     <hr>
                     <h2 class="intro-text text-center">The Perfect Cup
                         <strong>blog</strong>
@@ -182,3 +194,11 @@
 </body>
 
 </html>
+
+<?php 
+
+    } else{
+        header('Location: login.php');
+    }
+
+?>
